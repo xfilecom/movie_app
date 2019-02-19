b9f49c2a7b0997d683abd16aa40a58d73c748601
xfilecom <ahn.young.hoonnim@gmail.com>
2019-02-19 15:43:21 +0900

    Updates

---
+++ precache-manifest.9e161fd3c247d9d26076689ea82fa67e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7952d9d4b252ca1c580e",
+    "url": "/movie_app/static/js/runtime~main.7952d9d4.js"
+  },
+  {
+    "revision": "e6d5a98e056ce3cbc1e2",
+    "url": "/movie_app/static/js/main.e6d5a98e.chunk.js"
+  },
+  {
+    "revision": "c7b025323c5d87bdacfe",
+    "url": "/movie_app/static/js/2.c7b02532.chunk.js"
+  },
+  {
+    "revision": "e6d5a98e056ce3cbc1e2",
+    "url": "/movie_app/static/css/main.2e1f0911.chunk.css"
+  },
+  {
+    "revision": "4b4036b9ebcc4d91014831f1f1b53a2a",
+    "url": "/movie_app/index.html"
+  }
+];
